116727fb7c1ccfe807cbc87998572fe3cb073520
Vignesh11026309 <154620621+Vignesh11026309@users.noreply.github.com>
2023-12-29 11:51:38 +0530

    App.js

--- NTWIST-Clone/src/App.js
+++ NTWIST-Clone/src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import Header from './components/Header';
-// import Body from './components/Body';
 import Footer from './components/Footer';
 import MineToMill from './components/MineToMill'
 import Sustainability from './components/Sustainability'
@@ -12,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <Body/> */}
       <MineToMill/>
       <Sustainability/>
       <MineralProcessing/>
